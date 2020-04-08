6795da74d89cdc41dde68a312e47916edefe704f
kapiresz <63201604+kapiresz@users.noreply.github.com>
2020-04-08 16:13:30 +0100

    forgot to add

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@ library("data.table")
 
 setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
-#Reads in data from file then subsets data for specified dates
+#Reads in data from file then subsets data for specified dates.
 powerDT <- data.table::fread(input = "household_power_consumption.txt"
                              , na.strings="?"
 )
